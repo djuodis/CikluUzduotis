e39a3d987388e0b0229c64f96bdf53cb08741174
djuodis <deividas73245@gmail.com>
2023-11-19 05:52:58 +0200

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+// 1. Užduotis
+
+let vardas = 'Deividas';
+
+for (let i = 0; i < 10; i+= 1) {
+    console.log(vardas);
+  }
+
+
+
+
+// 2. Užduotis
+
+let kartai = 10;
+
+for (let i = 0; i < kartai; i++) {
+    console.log(vardas);
+}
+
+
+
+
+
+// 3. Užduotis
+
+for (let i = 0; i < kartai; i++) {
+    console.log(i + '. ' + vardas);
+}
+
+
+
+
+
+// 4. Užduotis
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
